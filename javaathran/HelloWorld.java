7fc03d0241d54d28f2ea866b7ab0d1bc22118f1f
DavidBareRosavelt <85147443+DavidBareRosavelt@users.noreply.github.com>
2021-06-01 16:14:31 +0800

    FileReadOnly?

--- javaathran/HelloWorld.java
+++ javaathran/HelloWorld.java
@@ -1,17 +1,7 @@
-/**
- * 
- */
 package test.workspace.javaathran;
 
-/**
- * @author Admin
- *
- */
 public class HelloWorld {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
